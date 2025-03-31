63965be4edc7815ecf6fe88d53be693fe01e9fb9
WINEZERO <46115146+winezer0@users.noreply.github.com>
2025-04-01 01:00:34 +0800

    Update function_none.php

--- tree-sitter-2025/php_demo/function_none.php
+++ tree-sitter-2025/php_demo/function_none.php
@@ -1,5 +1,5 @@
 <?php
-$back_type_arr=array('0'=>'退货-退回', '1'=>'<font color=#ff3300>换货-退回</font>', '2'=>'<font color=#ff3300>换货-换出</font>', '4'=>'退款-无需退货');
+$back_type_arr=array('0'=>'退货-退回', '1'=>'换货-退回', '2'=>'换货-换出', '4'=>'退款-无需退货');
 back_list_6666666();
 function back_action($back_id, $status_back, $status_refund,  $note = '', $username = null)
 {
