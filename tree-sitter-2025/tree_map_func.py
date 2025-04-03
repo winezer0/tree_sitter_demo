7db4089b57c783bbfe382498bd90c62c1974aab0
WINEZERO <kissloo@foxmail.com>
2025-04-03 09:14:57 +0800

    Update tree_map_func.py

--- tree-sitter-2025/tree_map_func.py
+++ tree-sitter-2025/tree_map_func.py
@@ -27,14 +27,18 @@ if __name__ == '__main__':
         print(f"read_file_to_parse:->{abspath_path}")
         # 分析函数信息
         method_infos = analyze_direct_method_infos(php_file_tree, LANGUAGE)
-        print(f"function_info:->{method_infos}")
+        print(f"method_infos:")
+        print_json(method_infos)
+
         # 分析类信息（在常量分析之后添加）
         class_infos = analyze_class_infos(php_file_tree, LANGUAGE)
-        print(f"class_info:->{class_infos}")
+        print(f"class_infos:")
+        print_json(class_infos)
+
         # 修改总结结果信息
         parsed_infos[abspath_path] = {METHOD_INFOS: method_infos, CLASS_INFOS: class_infos}
+    print(f"parsed_infos:")
     print_json(parsed_infos)
-
     # Dictionary to store parsed information
     analyzed_infos = analyze_func_relation(parsed_infos)
     print_json(analyzed_infos)
