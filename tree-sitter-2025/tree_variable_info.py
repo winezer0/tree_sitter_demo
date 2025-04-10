e194332ba1871bfa68487f8abc3b9e3204e1fc22
WINEZERO <kissloo@foxmail.com>
2025-04-10 18:55:55 +0800

    renaem

--- tree-sitter-2025/tree_variable_info.py
+++ tree-sitter-2025/tree_variable_info.py
@@ -157,7 +157,7 @@ if __name__ == '__main__':
     # php_file = r"php_demo/var_spuer_globals.php"
     # php_file = r"php_demo/var_globals.php"
     # php_file = r"php_demo/var_static.php"
-    php_file = r"php_demo/var_all.php"
+    php_file = r"php_demo/var_demo/var_all.php"
     # php_file = r"php_demo\class.php"
     root_node = read_file_to_root(PARSER, php_file)
     # 分析所有变量
